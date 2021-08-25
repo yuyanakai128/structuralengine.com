d820ed3c2b52f6863aa9c1b9ce0a39ba61006c01
sasaco <sasaicco@gmail.com>
2021-08-25 09:54:27 +0900

    href を routerLink に変更

--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { NewsService } from './news.service';
 
 @Component({
   selector: 'app-home-news',
@@ -8,64 +8,12 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 })
 export class HomeNewsComponent implements OnInit { 
 
-  public url: string = "https://asia-northeast1-the-structural-engine.cloudfunctions.net/note-rss";
   public rss_feeds: any[];
 
-  constructor(private http: HttpClient,){
-    this.rss_feeds = [];
-  }
+  constructor(private news: NewsService){ }
 
   ngOnInit() {
-
-    this.rss_feeds = [];
-
-    this.http.get(this.url, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
-      })
-    })
-    .subscribe(
-      response => {
-        // 通信成功時の処理（成功コールバック）
-        console.log('通信成功!!', response);
-        const res: any = response;
-        const feed = res.feed;
-        const entries = res.entries;
-        
-        const url = feed.link; // "https://note.com/structuralengine"
-        const icon = feed.webfeeds_icon;
-
-        for(const entry of entries){
-          const link = entry.link;
-          let thumbnail = entry.note_creatorimage;
-          if('media_thumbnail' in entry){
-            thumbnail = entry.media_thumbnail[0].url;
-          }
-          const title = entry.title;
-          const description = entry.summary;
-          const pubDate_y = entry.published_parsed[0];
-          const pubDate_m = entry.published_parsed[1];
-          const pubDate_d = entry.published_parsed[2];
-
-          this.rss_feeds.push({
-            link,
-            thumbnail,
-            title,
-            description,
-            pubDate_y,
-            pubDate_m,
-            pubDate_d
-          })
-        }
-
-      },
-      error => {
-        console.log(error);
-      }
-    );
-
-
+    this.rss_feeds = this.news.rss_feeds;
   }
 
 
