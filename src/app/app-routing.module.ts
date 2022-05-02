a7eb991c5120d0c77fdafeefe25c1148d73c1b3c
mizutani488 <kmizutani710@gmail.com>
2022-05-02 22:21:41 +0900

    フォーマルバージョンのホームページができた。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LawComponent } from './law/law.component';
-import { PolicyComponent } from './policy/policy.component';
-import { RuleComponent } from './rule/rule.component';
-import { StripeComponent } from './stripe/stripe.component';
-import { PriceComponent } from './price/price.component'
-import { EmploymentComponent } from './employment/employment.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LawComponent } from "./law/law.component";
+import { PolicyComponent } from "./policy/policy.component";
+import { RuleComponent } from "./rule/rule.component";
+import { StripeComponent } from "./stripe/stripe.component";
+import { PriceComponent } from "./price/price.component";
+import { EmploymentComponent } from "./employment/employment.component";
+import { FormalComponent } from "./formal/formal.component";
 
-const routes: Routes = [ 
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'law', component: LawComponent },
-  { path: 'policy', component: PolicyComponent },
-  { path: 'rule', component: RuleComponent },
-  { path: 'stripe', component: StripeComponent },
-  { path: 'price', component: PriceComponent },
-  { path: 'employment', component: EmploymentComponent }
+const routes: Routes = [
+  { path: "", redirectTo: "/formal", pathMatch: "full" },
+  { path: "formal", component: FormalComponent },
+  { path: "home", component: HomeComponent },
+  { path: "law", component: LawComponent },
+  { path: "policy", component: PolicyComponent },
+  { path: "rule", component: RuleComponent },
+  { path: "stripe", component: StripeComponent },
+  { path: "price", component: PriceComponent },
+  { path: "employment", component: EmploymentComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: false,
-    scrollPositionRestoration: 'enabled',
-    anchorScrolling: 'enabled'
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: false,
+      scrollPositionRestoration: "enabled",
+      anchorScrolling: "enabled",
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/news/news.service.ts
+++ src/app/home/news/news.service.ts
@@ -1,73 +1,69 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NewsService {
-
-  private url: string = "https://asia-northeast1-the-structural-engine.cloudfunctions.net/note-rss";
+  private url: string =
+    "https://asia-northeast1-the-structural-engine.cloudfunctions.net/note-rss";
   public rss_feeds: any[];
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
     this.rss_feeds = [];
 
-    this.http.get(this.url, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
+    this.http
+      .get(this.url, {
+        headers: new HttpHeaders({
+          "Content-Type": "application/x-www-form-urlencoded",
+          Accept: "application/json",
+        }),
       })
-    })
-    .subscribe(
-      response => {
-        // 通信成功時の処理（成功コールバック）
-        console.log('通信成功!!', response);
-        const res: any = response;
-        const entries = res.entries;
-        let counter = 0;
-        for(const entry of entries){
-          const link = entry.link;
-          let thumbnail = "./assets/parallax/SkyCiv-Suite-min.jpg";
-          for(const l of entry.links){
-            const t: string = l.type;
-            if(t.includes('image')){
-              thumbnail = l.href;
-              break;
+      .subscribe(
+        (response) => {
+          // 通信成功時の処理（成功コールバック）
+          console.log("通信成功!!", response);
+          const res: any = response;
+          const entries = res.entries;
+          let counter = 0;
+          for (const entry of entries) {
+            const link = entry.link;
+            let thumbnail = "./assets/parallax/SkyCiv-Suite-min.jpg";
+            for (const l of entry.links) {
+              const t: string = l.type;
+              if (t.includes("image")) {
+                thumbnail = l.href;
+                break;
+              }
+            }
+            if ("media_thumbnail" in entry) {
+              thumbnail = entry.media_thumbnail[0].url;
+            }
+
+            const title = entry.title;
+            const description = entry.summary;
+            const pubDate_y = entry.published_parsed[0];
+            const pubDate_m = entry.published_parsed[1];
+            const pubDate_d = entry.published_parsed[2];
+
+            this.rss_feeds.push({
+              link,
+              thumbnail,
+              title,
+              description,
+              pubDate_y,
+              pubDate_m,
+              pubDate_d,
+            });
+            counter++;
+            if (counter > 4) {
+              break; // 4つを上限とする
             }
           }
-          if('media_thumbnail' in entry) {
-            thumbnail = entry.media_thumbnail[0].url;
-          }
-
-          const title = entry.title;
-          const description = entry.summary;
-          const pubDate_y = entry.published_parsed[0];
-          const pubDate_m = entry.published_parsed[1];
-          const pubDate_d = entry.published_parsed[2];
-
-          this.rss_feeds.push({
-            link,
-            thumbnail,
-            title,
-            description,
-            pubDate_y,
-            pubDate_m,
-            pubDate_d
-          })
-          counter++;
-          if(counter>3){
-            break; // 4つを上限とする
-          }
+        },
+        (error) => {
+          console.log(error);
         }
-
-      },
-      error => {
-        console.log(error);
-      }
-    );
-
+      );
   }
-
-
-
 }
